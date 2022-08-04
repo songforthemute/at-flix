f574c0e4d7250a591990b3b73aba935a86455387
songforthemuto <huni0423@naver.com>
2022-08-05 01:36:12 +0900

    mobile inspector

--- src/libs.ts
+++ src/libs.ts
@@ -3,3 +3,17 @@ const IMG_URL = process.env.REACT_APP_IMG_URL;
 export function getImagePath(id: string, format?: string) {
     return `${IMG_URL}/${format ? format : "original"}/${id}`;
 }
+
+export function isMobile() {
+    const { userAgent } = window.navigator;
+
+    if (
+        userAgent.match(
+            /iPhone|iPod|Android|Windows CE|BlackBerry|Symbian|Windows Phone|webOS|Opera Mini|Opera Mobi|POLARIS|IEMobile|lgtelecom|nokia|SonyEricsson|LG|SAMSUNG|Samsung/i
+        )
+    ) {
+        return false;
+    } else {
+        return true;
+    }
+}
