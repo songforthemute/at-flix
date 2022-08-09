1be188997f6425280094759b32c36e79ffa8a099
songforthemuto <huni0423@naver.com>
2022-08-10 03:57:38 +0900

    react-query devtools

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from "./App";
 import GlobalStyle from "./theme/GlobalStyle";
 import { theme } from "./theme/theme";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const rootElement = document.getElementById("root");
 
@@ -17,6 +18,7 @@ render(
                 <GlobalStyle />
                 <App />
             </ThemeProvider>
+            <ReactQueryDevtools initialIsOpen={true} />
         </QueryClientProvider>
     </RecoilRoot>,
     rootElement
